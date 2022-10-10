8616aa2b7e3d3e15e79a94a987282111ccc90890
JhohannesK <Jdomeh77@gmail.com>
2022-10-10 19:08:05 +0000

    add member components

---
+++ pages/components/Member.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Props {
+	id: string;
+	name: string;
+	socialId: string;
+	link: string;
+}
+
+const Member: React.FC<Props> = ({ id, name, socialId, link }) => {
+	return (
+		<div>
+			<Image src='/ImageOne.svg' alt={name} width={1366} height={1555} />
+			<div className='text-2xl xl:text-3xl'>{name}</div>
+			<div className='text-xl'>
+				<Link href={link}>
+					<a href='' target='_blank'>
+						{socialId}
+					</a>
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Member;
