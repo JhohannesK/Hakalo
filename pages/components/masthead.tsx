e6f5780dbb1863e15d65a1e60715635c84539879
JhohannesK <Jdomeh77@gmail.com>
2022-10-20 09:47:10 +0000

    change masthead video

--- pages/components/masthead.tsx
+++ pages/components/masthead.tsx
@@ -51,9 +51,9 @@ const Masthead: React.FC = () => {
 				/>
 			</div>
 			<div className='p-12 font-bold z-10 text-lime-400 drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col '>
-				<h1 className='mb-6 text-4xl xl:text-5xl text-purple'>Hakalo</h1>
+				<h1 className='mb-6 text-4xl xl:text-7xl text-purple'>Hakalo</h1>
 				<h2 className='mb-2 text-2xl xl:text-3xl tracking-tight text-orange '>
-					<span>Real Estate </span>
+					<span>Universe of Devs </span>
 					<span>| Best in the world</span>
 				</h2>
 			</div>
